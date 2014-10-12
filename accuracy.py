9ebf2f9a546cf27b7495a7f258907b367516c0c5
viveknabhi <viveknabhi@gmail.com>
2014-10-12 13:24:40 -0400

    Updated the accuracy code

--- accuracy.py
+++ accuracy.py
@@ -2,7 +2,7 @@ import sklearn.metrics as metrics
 
 
 def getSilhoutteScore(data,labels,sampleSize,metric):
-	return  metrics.silhouette_score(data, labels,metric=metric,sample_size=len(data))
+	return  metrics.silhouette_score(data, labels,sample_size=len(data))
 
 
 def getAccuracy(data,labels,sampleSize,metric):
